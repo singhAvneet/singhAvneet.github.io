0867e60c9fdf545616d44f967a5b109c74b58f00
singhAvneet <asingh@gms.ca>
2021-06-29 11:58:12 -0600

    removing files

--- ext-src/extension.d.ts
+++
@@ -1,6 +0,0 @@
-import * as vscode from 'vscode';
-/**
- * Activates extension
- * @param context vscode extension context
- */
-export declare function activate(context: vscode.ExtensionContext): void;
